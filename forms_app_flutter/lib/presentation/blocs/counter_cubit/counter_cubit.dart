48c461c469cbe1bd529899816f872359070fc147
Joshua A. Diaz Robayna <jdiarob@saosl.com>
2023-12-12 15:16:41 +0000

    Update Equatable

--- forms_app_flutter/lib/presentation/blocs/counter_cubit/counter_cubit.dart
+++ forms_app_flutter/lib/presentation/blocs/counter_cubit/counter_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  CounterCubit() : super(CounterState(counter: 5));
+
+  void increaseBy(int value) {
+    emit(state.copyWith(
+      counter: state.counter + value,
+      transcaccionCount: state.transcaccionCount + 1,
+      ));
+    
+  }
+
+  void reset() {
+    emit(
+      state.copyWith(
+        counter: 0, 
+        ));
+  }
 }
--- forms_app_flutter/lib/presentation/blocs/counter_cubit/counter_state.dart
+++ forms_app_flutter/lib/presentation/blocs/counter_cubit/counter_state.dart
@@ -1,12 +1,14 @@
 part of 'counter_cubit.dart';
-
-class CounterState {
+class CounterState extends Equatable {
   final int counter;
   final int transcaccionCount;
 
-  CounterState({this.counter = 0, this.transcaccionCount = 0});
+  const CounterState({this.counter = 0, this.transcaccionCount = 0});
 
   copyWith({int? counter, int? transcaccionCount}) => CounterState(
       counter: counter ?? this.counter,
       transcaccionCount: transcaccionCount ?? this.transcaccionCount);
+
+  @override
+  List<Object> get props => [counter, transcaccionCount];
 }
